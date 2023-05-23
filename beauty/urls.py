4ea0642fd1f4b4d465e6d6d7a2a6bbca7e7c6b99
ars <xakars02@gmail.com>
2023-05-23 17:05:48 +0600

    connect button

--- beauty/urls.py
+++ beauty/urls.py
@@ -7,4 +7,5 @@ app_name = "beauty"
 
 urlpatterns = [
     path('', views.view_index, name='start_page'),
+    path('service/', views.view_service, name='service_page')
 ]
--- beauty/views.py
+++ beauty/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def view_index(request):
     return render(request, template_name="index.html", context={})
+
+
+def view_service(request):
+    return render(request, template_name="service.html", context={})
