bf2a1ebc784850070c396466fcecc4e95cd7bfce
ludwig300 <mr4silent@gmail.com>
2023-05-25 21:42:11 +0700

    Connected the database to the template

--- beauty/views.py
+++ beauty/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from beauty.models import Service, Master, Salon, Review
 
 
 def view_index(request):
-    return render(request, template_name="index.html", context={})
+    services = Service.objects.all()
+    salons = Salon.objects.all()
+    masters = Master.objects.all()
+    reviews = Review.objects.all()
+    return render(
+        request,
+        template_name="index.html",
+        context={
+            'salons': salons,
+            'services': services,
+            'masters': masters,
+            'reviews': reviews
+        }
+    )
 
 
 def view_service(request):
--- beauty_city/settings.py
+++ beauty_city/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -129,6 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'beauty/static/'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'staticfiles/'),
+# ]
